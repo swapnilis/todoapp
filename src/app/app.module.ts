db32f0faf8482437c1a8a60fd579befe0e0b8ae7
Swapnil <swapnilsingh315@gmail.com>
2023-11-03 12:39:22 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderSectionComponent } from './header-section/header-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    HeaderSectionComponent
   ],
   imports: [
     BrowserModule,
